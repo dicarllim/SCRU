36803e3f51c9fe18ce23170edaa4d23139a3296d
Ariany <arianysferreira1@gmail.com>
2015-11-29 11:38:03 -0300

    IRepositorioAluno adicionado (procurar e remover)

--- src/com/poo/dados/IRepositorioAluno.java
+++ src/com/poo/dados/IRepositorioAluno.java
@@ -3,13 +3,17 @@ package com.poo.dados;
 import java.io.IOException;
 
 import com.poo.excecoes.CadastroAlunoExistenteException;
+import com.poo.excecoes.ProcuraAlunoInexistenteException;
 import com.poo.negocios.beans.Aluno;
 
 public interface IRepositorioAluno {
 
-	boolean equals(Aluno a, Aluno B);
 	public boolean existe(Aluno aluno);
 	public void inserirAluno(Aluno aluno) throws IOException, CadastroAlunoExistenteException;
 	public Aluno[] listarAlunos();
+	public Aluno procurar(Aluno aluno)throws ProcuraAlunoInexistenteException;
+	public void remover(Aluno aluno) throws IOException, ProcuraAlunoInexistenteException;
+	boolean equals(Aluno a, Aluno B);
 	
 }
+
