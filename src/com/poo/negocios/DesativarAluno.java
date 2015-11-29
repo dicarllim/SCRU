8ef5ca98b1ac9eb8ed5db1c448040bcff485b9eb
Ariany <arianysferreira1@gmail.com>
2015-11-29 16:01:34 -0300

    Commit DesativarAluno

--- src/com/poo/negocios/DesativarAluno.java
+++ src/com/poo/negocios/DesativarAluno.java
@@ -1,5 +1,26 @@
 package com.poo.negocios;
 
+import java.io.IOException;
+
+import com.poo.dados.IRepositorioAluno;
+import com.poo.dados.RepositorioAluno;
+import com.poo.excecoes.ProcuraAlunoInexistenteException;
+import com.poo.negocios.beans.Aluno;
+
 public class DesativarAluno {
+	private IRepositorioAluno repositorio;
+	
+	public DesativarAluno(){
+		try{
+			this.repositorio = RepositorioAluno.getInstance();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void remover(Aluno aluno) throws IOException, ProcuraAlunoInexistenteException{
+		this.repositorio.remover(aluno);
+	}
+	
 
 }
