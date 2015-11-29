8340cb0ec001e1abad8ff750314c4a00f8f6f42c
Ariany <arianysferreira1@gmail.com>
2015-11-29 14:09:03 -0300

    Commit CadastroAluno

--- src/com/poo/negocios/CadastroAluno.java
+++ src/com/poo/negocios/CadastroAluno.java
@@ -1,5 +1,30 @@
 package com.poo.negocios;
 
+import java.io.IOException;
+
+import com.poo.dados.IRepositorioAluno;
+import com.poo.dados.RepositorioAluno;
+import com.poo.excecoes.CadastroAlunoExistenteException;
+import com.poo.negocios.beans.Aluno;
+
 public class CadastroAluno {
+	
+	private IRepositorioAluno repositorio;
+	
+	public CadastroAluno(){
+		try{
+			this.repositorio = RepositorioAluno.getInstance();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void cadastrar(Aluno aluno)throws IOException, CadastroAlunoExistenteException{
+		this.repositorio.inserirAluno(aluno);
+	}
+	
+	public Aluno[] listarAlunos() throws IOException{
+		return this.repositorio.listarAlunos();
+	}
 	
 }
