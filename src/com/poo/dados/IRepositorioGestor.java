ef99ded0af2f16c3e4148d0eefd4442273a0b0c1
Ariany <arianysferreira1@gmail.com>
2015-11-29 12:18:50 -0300

    IRepositorioGestor adicionado (procurar e remover)
    Tive que criar uma exceção ProcuraGestorInexistenteException.

--- src/com/poo/dados/IRepositorioGestor.java
+++ src/com/poo/dados/IRepositorioGestor.java
@@ -3,13 +3,16 @@ package com.poo.dados;
 import java.io.IOException;
 
 import com.poo.excecoes.CadastroGestorExistenteException;
+import com.poo.excecoes.ProcuraGestorInexistenteException;
 import com.poo.negocios.beans.Gestor;
 
 public interface IRepositorioGestor {
 
-	boolean equals(Gestor a, Gestor B);
 	public boolean existe(Gestor gestor);
 	public void inserirGestor(Gestor gestor) throws IOException, CadastroGestorExistenteException;
 	public Gestor[] listarGestores();
+	public Gestor procurar(Gestor gestor)throws ProcuraGestorInexistenteException;
+	public void remover(Gestor gestor)throws IOException, ProcuraGestorInexistenteException;
+	boolean equals(Gestor a, Gestor B);
 
 }
