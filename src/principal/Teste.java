b2fb2ab7158e61e982535ba89b4a7c512042752b
Fumigari <arianysferreira1@gmail.com>
2016-05-03 16:27:41 -0300

    Commit Teste

--- src/principal/Teste.java
+++ src/principal/Teste.java
@@ -12,6 +12,7 @@ public class Teste {
 		Gestor gestor = new Gestor("Ariany Ferreira","10561403481", "123456");
 		Aluno aluno = new Aluno("Maria Rebelo", "07354563480", "BCC", 2014.2f, 1646041125, 20.0, "03/12/2015");
 		Aluno aluno1 = new Aluno("Thales","11762468441","BCC", 2014.2f,1646041121,30,"05,12,2015");
+		//Aluno aluno2 = new Aluno("Olar", "10561402400", "BCC", 2014.2f, 16985775609, 30, "03/11/2015");
 	    
 		try {
 			Fachada.getInstance().cadastrar(aluno1);
